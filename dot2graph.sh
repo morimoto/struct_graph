dd0e9396eefb20668db6ac1b3e94233b9d0eafbb
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-07-26 10:11:25 +0900

    Add dot2graph
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

---
+++ dot2graph.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#===============================
+#
+# dot2graph
+#
+# 2016/07/26 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+
+FILE=$1
+[ ! -f $1 ] && echo "no such file" && exit
+
+dot -Teps $FILE -o $FILE.pdf 
